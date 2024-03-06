2dfe873238aface96ca0bb1fea7923de3dfe2158
suresh298877 <sureshpeddimsetti298@gmail.com>
2024-03-06 22:08:43 +0530

    alsdk

--- ECOMMERCE/inventory/models.py
+++ ECOMMERCE/inventory/models.py
@@ -14,12 +14,12 @@ class Product(models.Model):
     }
     pid=models.CharField(max_length=255)
     name=models.CharField(max_length=100)
-    slug=models.SlugField()
-    description=models.TextField()
-    is_digital=models.BooleanField()
+    slug=models.SlugField(unique=True)
+    description=models.TextField(null=True)
+    is_digital=models.BooleanField(default=False)
     created_at=models.DateTimeField(auto_now_add=True, editable=False)
     updated_at=models.DateTimeField(auto_now_add=False,editable=False)
-    is_active=models.BooleanField()
+    is_active=models.BooleanField(default=False)
     stock_status=models.CharField(
         max_length=3,
         choices=STOCK_STATUS,
@@ -29,8 +29,8 @@ class Product(models.Model):
 class ProductLine(models.Model):
     price=models.DecimalField()
     sku=models.UUIDField(default=uuid.uuid4)
-    stock_qty=models.IntegerField()
-    is_active=models.BooleanField()
+    stock_qty=models.IntegerField(default=0)
+    is_active=models.BooleanField(default=False)
     order=models.IntegerField()
     weight=models.FloatField()
 
@@ -42,10 +42,10 @@ class ProductImage(models.Model):
 
 class Category(models.Model):
     name=models.CharField(max_length=100)
-    slug=models.SlugField()
+    slug=models.SlugField(unique=True)
     is_active=models.BooleanField()
 
 class SeasonalEvents(models.Model):
     start_date=models.DateTimeField()
     end_date=models.DateTimeField()
-    name=models.CharField(max_length=100)
+    name=models.CharField(max_length=100,unique=True)
